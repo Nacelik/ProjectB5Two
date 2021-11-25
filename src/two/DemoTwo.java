bc5b81a53ebbfa6d6959d677d3da371e86b093a1
nihalcelik <nihalcelik@192.168.0.19>
2021-11-24 19:06:12 -0600

    isEqual method added...

--- src/two/DemoTwo.java
+++ src/two/DemoTwo.java
@@ -3,6 +3,16 @@ package two;
 public class DemoTwo {
 	public static void main(String[] args) {
 		System.out.println("Demo Two Code....");
+		
+		isEqual(7, 8);
 	}
 
-}
+	public static void isEqual(int a, int b) {
+		if( a == b) {
+			System.out.println("Values are equal");
+	}else 
+		System.out.println("Values are not equal");
+		}
+	}
+
+	
