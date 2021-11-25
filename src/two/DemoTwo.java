80cd2cc0db1a7fa142df46529c136f786f45ed48
nihalcelik <nihalcelik@192.168.0.19>
2021-11-24 19:08:48 -0600

    formatted isEqual method added...

--- src/two/DemoTwo.java
+++ src/two/DemoTwo.java
@@ -3,16 +3,14 @@ package two;
 public class DemoTwo {
 	public static void main(String[] args) {
 		System.out.println("Demo Two Code....");
-		
+
 		isEqual(7, 8);
 	}
 
 	public static void isEqual(int a, int b) {
-		if( a == b) {
+		if (a == b) {
 			System.out.println("Values are equal");
-	}else 
-		System.out.println("Values are not equal");
-		}
+		} else
+			System.out.println("Values are not equal");
 	}
-
-	
+}
